9b225f67f12927b01e0bf13cb562f2b38821fa55
EC2 Default User <ec2-user@ip-172-31-9-18.ap-northeast-1.compute.internal>
2023-02-05 12:07:35 +0000

    [Finish] userのupdate機能の作成まで　画像表示を修正済み

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,17 +12,19 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
+    @user = User.find(params[:id])
   end
-  
+
   def update
-    @book = Book.find(params[:id])
+    @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(id: @user.id)
   end
-  
-  
-  
-  
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
